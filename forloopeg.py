32430c772fe44abc7653406393c86ee9147e028e
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-15 22:50:50 +0200

    added new example

--- forloopeg.py
+++ forloopeg.py
@@ -38,12 +38,32 @@ print("even=",even,"\nodd=",odd)'''
 
 #Example for printing the numbers which are divisible by 3 and 5
 
-a=int(input("Enter a number: "))    
+"""a=int(input("Enter a number: "))    
 b=int(input("Enter a number: "))
 num3=0
 
 for i in range(a,b):
     if i%3==0 and i%5==0:
         num3=num3+1
-print("number divicible by 3 and 5 are =",num3)
+print("number divicible by 3 and 5 are =",num3)"""
+
+#Example 6 sum of first 5 natural numbers
+
+'''sum=0
+for i in range(1,5+1):
+    sum=sum+i
+print(sum)'''
+
+#Example 7 getting multiple using list integer and adding it
+
+a=[]
+for i in range(10):
+    num=int(input("Enter number"+str(i+1)))
+    a.append(num)
+print(a)
+
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
 
