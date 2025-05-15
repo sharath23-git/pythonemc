e3f8624aed9eb79c211caf3d3010ae04fbb05141
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-15 21:15:57 +0200

    added example 2

---
+++ forloopeg.py
@@ -0,0 +1,14 @@
+#example 1 printing numbers inbetween two numbers
+
+'''a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+for i in range(a+1,b):
+    print(i)'''
+
+#example 2 printing even numbers inbetween two numbers
+
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))    
+for i in range(a,g):
+    if i%2==0:
+        print(i)
