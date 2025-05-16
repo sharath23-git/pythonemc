4c2b6289983158712e2784b518c758ef62eff6f4
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-16 14:19:41 +0200

    ADD

--- whileloop.py
+++ whileloop.py
@@ -3,9 +3,22 @@ while(i==0):
     print(i)
     i=1'''
 
-num=int(input("Enter a number: "))
+'''num=int(input("Enter a number: "))
 i=0
 while(i<=num):
     print(i)
-    i=i+1
-    
+    i=i+1'''
+
+#Example to print 10,20...200
+
+'''i=0
+while(i<=200):
+    print(i,end=",")
+    i=i+10'''
+
+#Example to print first ten natural numbers in rewerse order
+
+'''i=10
+while(i>=1):
+    print(i,end=",")
+    i=i-1'''
