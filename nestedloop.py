d44fefdbefa31de5dc9dd096351486d7bbc6a978
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-16 13:29:38 +0200

    Added new example

--- nestedloop.py
+++ nestedloop.py
@@ -1,4 +1,20 @@
-for i in range (1,3):
+f'''or i in range (1,3):
     print("week: ",i)
     for j in range(1,4):
-        print("\tday:",j)
+        print("\tday:",j)'''
+
+#Example 1 star
+
+''''for i in range(1,6):
+    print()
+    for j in range(i):
+        print("*",end="")'''
+
+#Example 2 pyramid
+
+'''for i in range(1,6):
+    print()
+    for j in range(1,6-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end=" ")'''
