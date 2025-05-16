878327d06fc4001a28724f680fe11b8973d9eddb
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-16 14:29:08 +0200

    ADDED example

--- whileloop.py
+++ whileloop.py
@@ -22,3 +22,12 @@ while(i<=200):
 while(i>=1):
     print(i,end=",")
     i=i-1'''
+
+#Example to print factorial of a number
+
+'''i=int(input("Enter a number: "))
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1
+print(fact)'''
