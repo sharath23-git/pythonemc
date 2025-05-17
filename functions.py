bcb1e376c10d7791b567da926801a2344c2b2e64
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-17 18:52:31 +0200

    Added example

---
+++ functions.py
@@ -0,0 +1,25 @@
+'''////////////////////////////FUNCTIONS////////////////////////////////'''
+
+'''def painter():
+    print("This is a painter function.")
+painter()'''
+
+#Example to create functon to get a number add(),sub(),mul(),div()
+def getnum():
+    num=int(input("Enter a number1: "))    
+    return num
+def add(a,b):
+    print("Addition of number 1 and 2: ",a+b)
+def sub(a,b):
+    print("subraction of number 1 and 2: ",a-b)
+def mul(a,b):
+    print("Multiplicarion of number 1 and 2: ",a*b)
+def div(a,b):
+    print("Division of number 1 and 2: ",a//b)
+
+a=getnum()
+b=getnum()
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
