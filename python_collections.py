63a7b132b9ddb24b001ea3957eb032038c2f28db
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-17 17:09:38 +0200

    ADD

--- python_collections.py
+++ python_collections.py
@@ -6,3 +6,10 @@ a.insert(1,"box")
 a.append(45)
 print(a)'''
 
+#Example 2 modify the lish
+
+'''a=[1,2,3,4,5,6]
+print(a)
+a[0]=11  #inserting 11 in the place of 1
+a[1:3]=[22,33]  #inserting 22 and 33 in the place of 2 and 3
+print(a)'''
