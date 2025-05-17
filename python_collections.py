d32405b5a96fd5876f005a08dbc2561f4a820d80
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-17 17:38:30 +0200

    Added example

--- python_collections.py
+++ python_collections.py
@@ -1,3 +1,7 @@
+''' Python Collections  
+1.List[]'''
+
+
 #Example 1 insert in list using append() and insert()
 
 '''a=[1,2,3,4,5,"aaa","sss","bbb"]
@@ -14,10 +18,25 @@ a[0]=11  #inserting 11 in the place of 1
 a[1:3]=[22,33]  #inserting 22 and 33 in the place of 2 and 3
 print(a)'''
 
-# Example to remove from list using pop()
+# Example to remove from list using pop() and remove()
 
-a=[1,2,3,4,5,6]
+'''a=[1,2,3,4,5,6]
 print(a)
-a.pop(1)
+a.pop(1)  # to remove the element in the index
+a.remove(3)  #to remove the exact element using the element name
+print(a)'''
+
+#Example to merge the list using extend()
+
+'''a=[1,2,3,4]
+b=[4,5,6,7]
+a.extend(b)
+print(a)'''
+
+'''Python collections
+2. Tuple()'''
+
+a=(1,2,3,4,5)
 print(a)
-print(a)
+#a.append(6) # we cannot add any element to tuple
+
