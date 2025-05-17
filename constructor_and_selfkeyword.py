5fb8200523ba11eb51c211463723b44122a30500
sharath23-git <sharathrangasamy23@gmail.com>
2025-05-17 21:56:04 +0200

    Added new example

--- constructor_and_selfkeyword.py
+++ constructor_and_selfkeyword.py
@@ -1,4 +1,4 @@
-class lap:
+'''class lap:
    
     def __init__(self):
         self.name=""
@@ -23,4 +23,24 @@ hp.ram="8GB"
 hp.price=20000
 hp.processor="i5"
 hp.display()
-dell.display()
+dell.display()'''
+
+#Example
+class student:
+    def __init__(self):
+       self.name="sel"
+       self.regno="9876"
+    def display(self):
+        print("Name of the student: ",self.name)
+        print("Register number: ",self.regno)
+
+s1=student()
+s2=student()
+s1.name="sri"
+s1.regno="1234"
+s2.name="ram"
+s2.regno="5678"
+print(s1.name)
+print(s1.regno)
+s1.display()
+s2.display()
